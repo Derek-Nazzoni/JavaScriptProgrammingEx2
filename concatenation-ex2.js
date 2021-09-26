d01035652e8dd1a1bb6cc3fd9c103309b8f06bc4
Derek-Nazzoni <derek.nazzoni@gmail.com>
2021-09-26 17:21:47 -0300

    concatenation-ex2 Done

--- concatenation-ex2.js
+++ concatenation-ex2.js
@@ -3,3 +3,13 @@ JavaScript Programming Concatenation Exercise #2
 Strings and string functions
 Derek Nazzoni
 */
+let firstName = "Derek";
+let lastName = "Nazzoni";
+let age = 31;
+let city = "Santo Andre";
+let married = "true";
+let firstJob = "Burger King";
+let hourlyRate = "7.00";
+
+console.log('Hello ' + firstName + ' ' + lastName + '. You are ' + age + ' years old and were born in ' + city + ', and currently live in ' +
+ 'Moncton. \nYour first job was at ' + firstJob + ' making $' + hourlyRate + '/hour.\nI asked if you were married and you said ' + married + '.')
