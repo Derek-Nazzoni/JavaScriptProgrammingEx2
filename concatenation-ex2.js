88a644d08987a5f0f85b12860186e27886b11dcc
Derek-Nazzoni <derek.nazzoni@gmail.com>
2021-09-27 10:31:05 -0300

    Update concatenation-ex2.js

--- concatenation-ex2.js
+++ concatenation-ex2.js
@@ -13,3 +13,7 @@ let hourlyRate = "7.00";
 
 console.log('Hello ' + firstName + ' ' + lastName + '. You are ' + age + ' years old and were born in ' + city + ', and currently live in ' +
  'Moncton. \nYour first job was at ' + firstJob + ' making $' + hourlyRate + '/hour.\nI asked if you were married and you said ' + married + '.')
+
+ console.log(`\nHello ${firstName} ${lastName}. You are ${age} years old and were born in ${city}, and currently live in Moncton.
+Your first job was at ${firstJob} making $${hourlyRate}/hour.
+I asked if you were married and you said ${married}.`)
